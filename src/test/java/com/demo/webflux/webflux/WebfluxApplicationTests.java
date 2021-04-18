11005d27a06d2a4e51d1e457450f302eefee0112
Kylian Gehier <gehier.k@sfeir.com>
2021-04-18 23:37:43 +0200

    fist commit

---
+++ src/test/java/com/demo/webflux/webflux/WebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.webflux.webflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
